fdbb5fc4def7d87c9c3f88a8116078a6cf1af8a5
TKH Student 1 <tkh@TKHs-Air-2.home>
2020-09-29 19:33:25 -0400

    first commit

--- conditionals.js
+++ conditionals.js
@@ -6,8 +6,16 @@
 // and log the output to make sure it works 
 //(e.g. "The greater number of 5 and 10 is 10")
 
+function greaterNum(num1,num2) {
+    if(num1 > num2){
+        console.log(`The greater number of ${num1} and ${num2} is ${num1}`);
+    } else {
+        console.log(`The greater number of ${num2} and ${num1} is ${num2}`);
+    }
+}
 
-
+greaterNum(5,10);
+greaterNum(100,7);
 
 
 
@@ -19,9 +27,29 @@
 //Call that function for each of the supported languages and log the 
 // resultto make sure it works
 
+function helloWorld(lanCode) {
+    switch (lanCode) {
+        case 'es':
+            console.log("Hola Mundo");
+            break;
+        
+        case 'de':
+            console.log("Hallo Welt");
+            break;
 
+        case 'en':
+                console.log("Hello World");
+                break;
 
+        default:
+            console.log("Hello World");
+    } 
+}
 
+helloWorld('es');
+helloWorld('de');
+helloWorld('en');
+helloWorld('');
 
 
 
@@ -32,12 +60,35 @@
 //Call that function for a few different scores and log the result 
 //to make sure it works
 
+function assignGrade(score) {
+    switch (true) {
+        case (score <= 100 && score >= 90):
+            console.log("A");
+            break;
+        case (score <= 89 && score >= 80):
+            console.log("B");
+            break;
+        case (score <= 79 && score >= 70):
+            console.log("C");
+            break;
+        case (score <= 69 && score >= 60):
+            console.log("D");
+            break;
+        case (score <= 59 && score >= 0):
+            console.log("F");
+            break;
+        default:
+            console.log("Invalid score");
+    }
+}
 
-
-
-
-
-
+assignGrade(97);
+assignGrade(87);
+assignGrade(77);
+assignGrade(67);
+assignGrade(37);
+assignGrade(0);
+assignGrade(101);
 
 
 //Exercise #4
@@ -47,3 +98,14 @@
 //call that function for a few different scores
 //and log the result to make sure it works
 //Bonus: Make it handle a few collective nouns like "sheep" and "geese"
+
+function pluralize(noun, number) {
+    if (number != 1 && noun != 'sheep' && noun != 'geese') {
+        return number + ' ' + noun + 's';
+    } else {
+        return number + ' ' + noun;
+    }
+}
+console.log('I have ' + pluralize('cat', 0));
+console.log('I have ' + pluralize('cat', 1));
+console.log('I have ' + pluralize('cat', 2));
