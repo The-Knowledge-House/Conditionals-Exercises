1e48c24d05811080e2c09c554e69f61ad979b739
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2020-09-28 22:27:48 -0400

    Conditional Exercises

--- conditionals.js
+++ conditionals.js
@@ -6,9 +6,14 @@
 // and log the output to make sure it works 
 //(e.g. "The greater number of 5 and 10 is 10")
 
-
-
-
+function greaterNum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+console.log(greaterNum(5, 10));
 
 
 //Exercise #2
@@ -19,9 +24,18 @@
 //Call that function for each of the supported languages and log the 
 // resultto make sure it works
 
-
-
-
+function helloWorld(lang) {
+  if (lang == 'fr') {
+    return 'Bonjour tout le monde';
+  } else if (lang == 'es') {
+    return 'Hola, Mundo';
+  } else {
+    return 'Hello, World';
+  }
+}
+console.log(helloWorld('en'));
+console.log(helloWorld('fr'));
+console.log(helloWorld('es'));
 
 
 
@@ -33,8 +47,21 @@
 //to make sure it works
 
 
-
-
+function assignGrade(score) {
+  if (score > 90) {
+    return 'A';
+  } else if (score > 80) {
+    return 'B';
+  } else if (score > 70) {
+    return 'C';
+  } else if (score > 65) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
+console.log('You got a ' + assignGrade(95));
+console.log('You got a ' + assignGrade(65));
 
 
 
@@ -47,3 +74,14 @@
 //call that function for a few different scores
 //and log the result to make sure it works
 //Bonus: Make it handle a few collective nouns like "sheep" and "geese"
+
+function pluralize(noun, number) {
+  if (number != 1 && noun != 'sheep' && noun != 'geese') {
+    return number + ' ' + noun + 's';
+  } else {
+    return number + ' ' + noun;
+  }
+}
+console.log('I have ' + pluralize('cat', 0));
+console.log('I have ' + pluralize('cat', 1));
+console.log('I have ' + pluralize('cat', 2));
