f5cf44ba285d44d8d5c7026838eff72701fbec93
TKH STUDENT 12 <tkhstudent12@TKHs-Air-3.home>
2020-09-28 21:44:40 -0400

    Completed classword - Naeem Shafi

--- conditionals.js
+++ conditionals.js
@@ -7,7 +7,15 @@
 //(e.g. "The greater number of 5 and 10 is 10")
 
 
+function greaterNum(a,b){
+    if ( a>b){
+        console.log("A is greater than B");
+    } else {
+        console.log("B is greater than A");
+    }
+}
 
+greaterNum(4,5);
 
 
 
@@ -17,12 +25,41 @@
 // returns "Hello, World" for the given language, for at least 
 // 3 languages. It should default to returning English
 //Call that function for each of the supported languages and log the 
-// resultto make sure it works
+// resultto make sure it works.
+/**
+function helloWorld(arg1){
+    switch (arg1) {
+        case 'es':
+            arg1 = "Hola Mundo";
+            console.log(arg1);
+            break;
+        case 'de':
+            arg1 = "Hallo Welt";
+            console.log(arg1);
+            break;
+        default:
+            arg1 = "Hello World";
+            console.log(arg1);
+      }
+} 
+helloWorld(en);
 
+*/
 
-
-
-
+function helloWorld(lang) {
+    if (lang == 'de') {
+      console.log("Hallo Welt");
+    } else if (lang == 'es') {
+      console.log("Hola Mundo");
+    } else {
+      console.log("Hello World");
+    }
+  }
+  
+  console.log(helloWorld('de'));
+  
+  
+  
 
 
 //Exercise #3
@@ -33,7 +70,21 @@
 //to make sure it works
 
 
+function assignGrade(numScore){
+    if (numScore < 60){
+        console.log("The grade is F");
+    } else if (numScore >= 61 && numScore <= 70 ){
+        console.log("The grade is D");
+    } else if ( numScore >= 71 && numScore <= 80 ){
+        console.log("The grade is C");
+    } else if ( numScore >= 81 && numScore <= 90 ){
+        console.log("The grade is B");
+    }else if ( numScore >= 91 ){
+        console.log("The grade is A");
+    }
+}
 
+assignGrade(70);
 
 
 
@@ -47,3 +98,13 @@
 //call that function for a few different scores
 //and log the result to make sure it works
 //Bonus: Make it handle a few collective nouns like "sheep" and "geese"
+
+function pluralize(noun,number){
+    if (number > 1){
+    console.log(`${number} ${noun}s`);
+    } else{
+        console.log(`${number} ${noun}`);
+    }
+}
+
+pluralize("cat",5);
