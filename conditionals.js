cb2c4d6b8274034d16d2e2582b4aed9e09df7e0b
NashidC <nash.chowdhury@gmail.com>
2020-09-28 21:32:46 -0400

    exercises completed

--- conditionals.js
+++ conditionals.js
@@ -6,7 +6,18 @@
 // and log the output to make sure it works 
 //(e.g. "The greater number of 5 and 10 is 10")
 
-
+function greaterNum(num1, num2) {
+  let higherNumber;
+  if (num1 > num2) {
+    higherNumber = num1;
+    console.log(`The greater number of ${num1} and ${num2} is ${higherNumber}`)
+  } else {
+    higherNumber = num2;
+    console.log(`The greater number of ${num1} and ${num2} is ${higherNumber}`)
+  }
+}
+greaterNum(3, 4)
+greaterNum(5, 4)
 
 
 
@@ -19,8 +30,17 @@
 //Call that function for each of the supported languages and log the 
 // resultto make sure it works
 
+function helloWorld(langCode) {
+  if (langCode === "es") {
+    console.log("Hola Mundo");
+  } else if (langCode === "de") {
+    console.log("Hallo Welt");
+  } else {
+    console.log("Hello World");
+  }
+}
 
-
+helloWorld("de")
 
 
 
@@ -47,3 +67,12 @@
 //call that function for a few different scores
 //and log the result to make sure it works
 //Bonus: Make it handle a few collective nouns like "sheep" and "geese"
+
+function pluralize(noun, num) {
+  if (num > 1) {
+    console.log(`${num} ${noun}s`);
+  } else {
+    console.log(`${num} ${noun}`);
+  }
+}
+pluralize("cat", 3)
