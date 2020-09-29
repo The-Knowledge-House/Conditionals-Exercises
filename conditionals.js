1ad74a72cf69982831f3acae600caa1f740559a1
david espinal <davidespinal@davids-MacBook-Pro.local>
2020-09-28 21:59:24 -0400

    Complete question #1 & #2

--- conditionals.js
+++ conditionals.js
@@ -1,44 +1,46 @@
 //Exercise #1
 //Write a function named greaterNum that:
 // takes 2 arguments, both numbers.
-// returns whichever number is the greater (higher) number. 
+// returns whichever number is the greater (higher) number.
 // Call that function 2 times with different number pairs
-// and log the output to make sure it works 
+// and log the output to make sure it works
 //(e.g. "The greater number of 5 and 10 is 10")
 
-
-
-
-
+function greaterNum(num1, num2) {
+  if (num1 > num2) {
+    console.log(`The geater number of ${num1} and ${num2} is ${num1} `);
+  } else {
+    console.log(`The geater number of ${num1} and ${num2} is ${num2} `);
+  }
+}
 
 //Exercise #2
-//Write a function named helloWorld that: 
+//Write a function named helloWorld that:
 // takes 1 argument, a language code ("es", "de", "en")
-// returns "Hello, World" for the given language, for at least 
+// returns "Hello, World" for the given language, for at least
 // 3 languages. It should default to returning English
-//Call that function for each of the supported languages and log the 
-// resultto make sure it works
+//Call that function for each of the supported languages and log the
+// result to make sure it works
 
-
-
-
-
-
+function helloWord(lang) {
+  switch (lang) {
+    case "es":
+      console.log("Hola mundo?");
+      break;
+    case "de":
+      console.log("Hallo Welt?");
+      break;
+    default:
+      console.log("Hello world");
+  }
+}
 
 //Exercise #3
 //Write a function named assignGrade that:
 //takes 1 argument, a number score
 //returns a grade for the score, either "A", "B", "C", "D", or "F"
-//Call that function for a few different scores and log the result 
+//Call that function for a few different scores and log the result
 //to make sure it works
-
-
-
-
-
-
-
-
 
 //Exercise #4
 //Write a function named pluralize that:
@@ -47,3 +49,13 @@
 //call that function for a few different scores
 //and log the result to make sure it works
 //Bonus: Make it handle a few collective nouns like "sheep" and "geese"
+
+function pluralize(noun, num) {
+  if (num != 1 && noun != "sheep" && noun != "geese") {
+    return num + " " + noun + "s";
+  } else {
+    return num + " " + noun;
+  }
+}
+
+console.log("I have " + pluralize("cat", 0));
