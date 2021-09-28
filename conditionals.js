0630ab1391dd60efcf05185ff9a17456484c8ed7
TKH Student 36 <contactbrandos@gmail.com>
2021-09-27 22:38:05 -0400

    Assignment

--- conditionals.js
+++ conditionals.js
@@ -6,6 +6,16 @@
 // and log the output to make sure it works 
 //(e.g. "The greater number of 5 and 10 is 10")
 
+function greaterNum(num1,num2){
+       if (num1 > num2){
+         return num1
+       }
+      else if(num2 > num1){
+         return num2
+       }
+     }
+     console.log(greaterNum(60,40))
+     console.log(greaterNum(40,20))
 
 
 
@@ -19,6 +29,23 @@
 //Call that function for each of the supported languages and log the 
 // resultto make sure it works
 
+function helloWorld(argu){
+       if (argu == "es"){
+         return "Hola, Planeta"
+       }
+       else if(argu == "de"){
+         return "Hallo Wereld"
+       }
+       else if(argu == "en"){
+         return "Hello, World"
+       }
+       else{
+         return "Hello, World"
+       }
+     }
+     helloWorld("de");
+     helloWorld("en");
+    helloWorld("es");
 
 
 
@@ -32,7 +59,16 @@
 //Call that function for a few different scores and log the result 
 //to make sure it works
 
-
+function greaterNum(num1,num2){
+       if (num1 > num2){
+         return num1
+       }
+       else if(num2 > num1){
+         return num2
+       }
+     }
+     console.log(greaterNum(60,40))
+     console.log(greaterNum(40,20))
 
 
 
@@ -47,3 +83,18 @@
 //call that function for a few different scores
 //and log the result to make sure it works
 //Bonus: Make it handle a few collective nouns like "sheep" and "geese"
+
+function pluralize(noun,num){
+    if (noun == "sheep" || noun == "geese"){
+      return noun
+    }
+    else if(num === 1){
+      return num + " " + noun
+    }
+    else if (num > 1){
+      let a = num + " "+ noun + "s"
+      return a
+    }
+  }
+  console.log(pluralize("bat", 20))
+  console.log(pluralize("banana", 12))
